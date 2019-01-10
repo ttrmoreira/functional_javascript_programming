57660e9f4a6dd415eb1e40a19f8176571d807008
ttrmoreira <ttrmoreira@gmail.com>
2019-01-09 22:29:39 -0200

    Uso de Mônaga como functor para verificações que seriam repetidas várias vezes.

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,8 @@
 import {handleStatus, log, timeoutPromise, retry} from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import {notasService as service} from './nota/service.js';
-import {takeUntil, debounceTime, pipe, partialize} from './utils/operators.js'
-
+import {takeUntil, debounceTime, pipe, partialize} from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -13,7 +13,7 @@ const operations = pipe(
 
 const action = operations (() => 
         retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143'))) 
-        .then(console.log)
+        .then(total => EventEmitter.emit('itensTotalizados', total))
         .catch(console.log)
 );
 
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,18 +1,20 @@
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, pipe } from '../utils/operators.js';
+import { Maybe } from '../utils/maybe.js';
 
 
 const API = `http://localhost:3000/notas`;
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo === code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
 
     listAll() {
         return fetch(API)
         .then(handleStatus)
+        .then(notas => Maybe.of(notas))
         .catch(err => {
             console.log(err);
             return Promise.reject('Não foi possível obter as notas fiscais');
@@ -27,7 +29,9 @@ export const notasService = {
         const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
         
         // realizando a composição
-        return this.listAll().then(sumItems);
+        return this.listAll()
+        .then(sumItems)
+        .then(result => result.getOrElse(0));
     }
 };
 
